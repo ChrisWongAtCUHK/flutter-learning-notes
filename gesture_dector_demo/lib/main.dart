9bc7f1abe15be1d82dbab865655777acc1e86491
Chris Wong <chris.wong.ksl@gmail.com>
2026-07-20 14:20:37 +0800

    Gemini GestureDetector Demo

--- gesture_dector_demo/lib/main.dart
+++ gesture_dector_demo/lib/main.dart
@@ -30,92 +30,110 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GestureDetectorDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GestureDetectorDemo extends StatefulWidget {
+  const GestureDetectorDemo({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GestureDetectorDemo> createState() => _GestureDetectorDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GestureDetectorDemoState extends State<GestureDetectorDemo> {
+  String _statusMessage = 'Interact with the card!';
+  Color _cardColor = Colors.indigo;
+  double _scale = 1.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // Dragging coordinates
+  Offset _cardOffset = Offset.zero;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: const Text('GestureDetector Demo')),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _statusMessage,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 40),
+
+            // Transform allows us to visually translate (drag) and scale the box
+            Transform.translate(
+              offset: _cardOffset,
+              child: Transform.scale(
+                scale: _scale,
+                child: GestureDetector(
+                  // 1. Single Tap
+                  onTap: () {
+                    setState(() {
+                      _statusMessage = 'Single Tapped!';
+                      _cardColor = Colors.indigo;
+                    });
+                  },
+
+                  // 2. Double Tap
+                  onDoubleTap: () {
+                    setState(() {
+                      _statusMessage = 'Double Tapped! (Toggled Scale)';
+                      _scale = _scale == 1.0 ? 1.2 : 1.0;
+                    });
+                  },
+
+                  // 3. Long Press
+                  onLongPress: () {
+                    setState(() {
+                      _statusMessage = 'Long Pressed! (Color Changed)';
+                      _cardColor = Colors.teal;
+                    });
+                  },
+
+                  // 4. Pan / Drag (Tracks finger movement)
+                  onPanUpdate: (DragUpdateDetails details) {
+                    setState(() {
+                      _statusMessage = 'Dragging...';
+                      // Update box position by adding movement delta
+                      _cardOffset += details.delta;
+                    });
+                  },
+
+                  // Reset position on drag end
+                  onPanEnd: (_) {
+                    setState(() {
+                      _statusMessage = 'Drag Ended!';
+                    });
+                  },
+
+                  // The Child Widget being wrapped
+                  child: Container(
+                    width: 160,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      color: _cardColor,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.black26, blurRadius: 10),
+                      ],
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Touch Me\n(Tap / Drag)',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
